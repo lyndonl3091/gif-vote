452cc56966a4932f67ad75f485e7a9b122c8eec2
RexCorpDev <rexcorpfx@gmail.com>
2016-07-08 19:30:20 -0700

    wrote calculate winner

--- public/js/mainController.js
+++ public/js/mainController.js
@@ -3,11 +3,45 @@
 angular.module('starkC')
 .controller('mainController', function($scope, ngSocket) {
 
-  ngSocket.on('mainCtrl', data=>{
-    console.log('FR: Backend \n', data);
-  })
+  // ngSocket.on('mainCtrl', data=>{
+  //   console.log('FR: Backend \n', data);
+  // })
+  //
+  // let msg = 'Backend, Front: TEST';
+  // ngSocket.emit('backEnd', msg);
 
-  let msg = 'Backend, Front: TEST';
-  ngSocket.emit('backEnd', msg);
+  // get new image
+  ngSocket.on('newImage', image =>{
+    $scope.$broadcast('newImage');
+    // render the image to the DOM
+  });
 
+  // submit hash tag
+  let hashTag = {
+    hashTag : '',
+    vote    : 0,
+    voters  : [],
+    id      : ''
+  };
+  ngSocket.emit('hashtag', {hashtag});
+
+  // get all hash tags after submission
+  ngSocket.on('allHashTags', allHashtags =>{
+    $scope.$broadcast('getAllTags');
+    // render all hash tags to DOM
+
+  //vote on hashTag
+  let newVote = {votedHashtag}
+  ngSocket.emit('vote', newVote);
+
+
+  ngSocket.on('newVote', allVotes =>{
+    // render that vote to the dom.
+    $scope.$broadcast('newVote')
+  });
+
+  ngSocket.on('winner', winner=>{
+    // check this user's uuid for winner match
+    $scope.$broadcast('winner');
+  });
 });
--- server/socket.js
+++ server/socket.js
@@ -3,13 +3,34 @@
 const io = require('socket.io');
 
 exports.init = (io, socket) => {
-  socket.on('backEnd', data => {
-    console.log('FR: FrontEnd\n', data);
+  let newImage,
+      winner,
+      allTags = [];
+
+  // socket.on('backEnd', data => {
+  //   console.log('FR: FrontEnd\n', data);
+  // });
+  //
+  // let testBackEnd = 'FrontEnd, BackEnd: TEST';
+  // socket.emit('mainCtrl', testBackEnd);
+
+  socket.emit('newImage', newImage);
+
+  socket.on('hashtag', data=>{
+    allTags.push(data.hashtag);
+    socket.broadcast('allHashTags', allTags);
   });
 
-  let testBackEnd = 'FrontEnd, BackEnd: TEST';
-  socket.emit('mainCtrl', testBackEnd);
+  socket.on('vote', data=>{
+    let votedOn = allTags[allTags.indexOf(data.hashtag)];
+    votedOn.vote += 1;
+    //re-broadcast new vote to all users
+    socket.broadcast('newVote', allTags);
+  });
 
-
-
+  let findWinner = allTags =>{
+    let winner = allTags[0];
+    allTags.forEach(hashtag=> hashTag.vote > winner.vote ? winner = hashTag : null );
+    socket.broadcast('winner', winner.id);
+  };
 };
